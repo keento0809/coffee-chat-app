5fd793218b33c64f61adac8fd8fcb0a8cea9500c
KEENTO <handball0321@gmail.com>
2024-06-09 02:17:11 +0900

    Fix: detailed styles

--- app/components/common/button/LinkButton/LinkButton.tsx
+++ app/components/common/button/LinkButton/LinkButton.tsx
@@ -9,13 +9,11 @@ type LinkButtonProps = {
 
 export const LinkButton: FC<LinkButtonProps> = ({ text, linkURL = "#" }) => {
   return (
-    <Button asChild>
-      <Link
-        href={linkURL}
-        className="rounded-md bg-indigo-600 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        {text}
-      </Link>
+    <Button
+      asChild
+      className="rounded-md bg-indigo-600 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      <Link href={linkURL}>{text}</Link>
     </Button>
   );
 };
